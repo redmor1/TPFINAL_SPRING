db7b223430578e7393ba8532f78a4560324d8da6
redmor1 <lightz17@hotmail.com>
2023-07-24 19:08:27 -0300

    Crear implementacion de JuegoService

---
+++ src/main/java/com/informatorio/tpfinalspring/service/juego/impl/JuegoServiceJPAImpl.java
@@ -0,0 +1,18 @@
+package com.informatorio.tpfinalspring.service.juego.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.informatorio.tpfinalspring.domain.Juego;
+import com.informatorio.tpfinalspring.mapper.juego.JuegoMapper;
+import com.informatorio.tpfinalspring.model.dto.juego.JuegoDTO;
+import com.informatorio.tpfinalspring.repository.juego.JuegoRepository;
+import com.informatorio.tpfinalspring.service.juego.JuegoService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class JuegoServiceJPAImpl implements JuegoService {
+
+}
