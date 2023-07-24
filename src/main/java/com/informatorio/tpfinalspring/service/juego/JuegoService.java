fc4eef2c3cccc777671b02bc15bc3a3d068f2feb
redmor1 <lightz17@hotmail.com>
2023-07-24 19:07:48 -0300

    Crear interfaz de JuegoService

---
+++ src/main/java/com/informatorio/tpfinalspring/service/juego/JuegoService.java
@@ -0,0 +1,8 @@
+package com.informatorio.tpfinalspring.service.juego;
+
+import com.informatorio.tpfinalspring.domain.Juego;
+import com.informatorio.tpfinalspring.model.dto.juego.JuegoDTO;
+
+public interface JuegoService {
+
+}
